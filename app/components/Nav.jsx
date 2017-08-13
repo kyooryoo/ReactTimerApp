8472a40f302973e60a46aa8841796b8be36578d3
Ling Jiang <kyooryoo.hotmail.com>
2017-08-13 12:42:49 -0400

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Nav = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Timer</li>
+                    <li>
+                        <IndexLink 
+                            to="/" 
+                            activeClassName="active-link" 
+                            activeStyle={{fontWeight: 'bold'}}
+                        >
+                            Timer
+                        </IndexLink>
+                    </li>
+                    <li>
+                        <Link 
+                            to="/countDown" 
+                            activeClassName="active-link" 
+                            activeStyle={{fontWeight: 'bold'}}
+                        >
+                            CountDown
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="https://github.com/kyooryoo" target="_blank">Ling</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Nav;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Timer</h1>
+                <p>clock component</p>
+                <p>controls component</p>
+            </div>
+        )
+    }
+});
+
+module.exports = Timer;
